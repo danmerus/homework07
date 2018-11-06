c9a096ba0aa3590962666d816f801fa66ae1c806
danmerus <leoghost@mail.ru>
2018-11-06 13:47:37 +0500

    v1

--- src/main/scala/fintech/homework07/Sorting.scala
+++ src/main/scala/fintech/homework07/Sorting.scala
@@ -1,5 +1,6 @@
 package fintech.homework07
-
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 /**
   * Реализовать алгоритмы quick-sort и merge-sort
   *  использую *подходящие* *мутабельные* коллекции
@@ -7,8 +8,57 @@ package fintech.homework07
 
 object Sorting {
 
-//  def mergeSort(???)
-//
-//  def quickSort(???)
+  def mergeSort[T](collection: mutable.ListBuffer[T])(implicit ord: T => Ordered[T]): mutable.ListBuffer[T] = {
+    def merge(right:mutable.ListBuffer[T], left:mutable.ListBuffer[T]): mutable.ListBuffer[T] = {
+      val res = ListBuffer[T]()
+      val size = right.length + left.length
+      while (res.length < size) {
+        if (left.isEmpty) {
+          res ++= right
+        }
+        if (right.isEmpty) {
+          res ++= left
+        }
+        if (left.nonEmpty && right.nonEmpty) {
+          if (left.head < right.head){
+          res += left.head
+          left.remove(0)
+        } else {
+          res += right.head
+          right.remove(0)
+        }
+      }}
+      res
+    }
 
+    if (collection.length > 1) {
+      val split = collection.splitAt(collection.length/2)
+      val left = mergeSort(split._1)
+      val right = mergeSort(split._2)
+      val res = merge(left, right)
+      res
+    }
+    else
+    {
+      collection
+    }
+  }
+
+  def quickSort[T](collection: mutable.ListBuffer[T])(implicit ord: T => Ordered[T]): mutable.ListBuffer[T] = {
+      if (collection.length <= 1) {
+        collection
+      }
+      else {
+        val elem = collection(collection.length / 2)
+        quickSort(collection.filter((x)=> x < elem)) ++
+          collection.filter(elem ==) ++
+          quickSort(collection.filter((x)=> x > elem))
+      }
+    }
+
+
+  def main(args: Array[String]): Unit = {
+    println(mergeSort(mutable.ListBuffer(2,1,6,4,5,3)))
+    println(quickSort(mutable.ListBuffer(2,1,6,4,5,3)))
+  }
 }
--- src/test/scala/fintech/homework07/SortingSpec.scala
+++ src/test/scala/fintech/homework07/SortingSpec.scala
@@ -1,6 +1,19 @@
 package fintech.homework07
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.mutable
+
 class SortingSpec extends FlatSpec with Matchers {
-  ???
+  it should "merge sort orrectly 1" in {
+    Sorting.mergeSort(mutable.ListBuffer(2,1,6,4,5,3)) shouldEqual mutable.ListBuffer(1,2,3,4,5,6)
+  }
+  it should "merge sort orrectly 2" in {
+    Sorting.mergeSort(mutable.ListBuffer('c','a','b')) shouldEqual mutable.ListBuffer('a','b','c')
+  }
+  it should "quick sort orrectly 1" in {
+    Sorting.quickSort(mutable.ListBuffer(2,1,6,4,5,3)) shouldEqual mutable.ListBuffer(1,2,3,4,5,6)
+  }
+  it should "quick sort orrectly 2" in {
+    Sorting.quickSort(mutable.ListBuffer('c','a','b')) shouldEqual mutable.ListBuffer('a','b','c')
+  }
 }
